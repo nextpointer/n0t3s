6677d1ee1fe0dfa38cb6a02c54b11c27b77a1282
nextpointer <110530249+nextpointer@users.noreply.github.com>
2025-07-30 16:11:25 +0530

    feat: add select for tagsearch and time filter

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,5 +1,16 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 import { Github, NotebookPen } from "lucide-react";
+import { useState, useEffect } from "react";
 
 export default function Home() {
   return (
@@ -14,6 +25,29 @@ export default function Home() {
           <Button>
             <NotebookPen className="mr-1" /> New Note
           </Button>
+        </div>
+        <div className="flex flex-row w-full gap-2 mt-12 justify-start items-center">
+          <Select>
+            <SelectTrigger className="w-[130px]">
+              <SelectValue placeholder="Filter by tag" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                <SelectItem value="all">All tags</SelectItem>
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+          <Select>
+            <SelectTrigger className="w-[150px]">
+              <SelectValue placeholder="Sort by date" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                <SelectItem value="newest">Newer first</SelectItem>
+                <SelectItem value="oldest">Older first</SelectItem>
+              </SelectGroup>
+            </SelectContent>
+          </Select>
         </div>
       </div>
     </>
