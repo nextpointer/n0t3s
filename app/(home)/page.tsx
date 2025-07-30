32bb815929aaf6a69a2848260ead2f48727a41cb
nextpointer <110530249+nextpointer@users.noreply.github.com>
2025-07-30 13:23:16 +0530

    feat: add header section

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,13 +1,21 @@
 import { Button } from "@/components/ui/button";
+import { Github, NotebookPen } from "lucide-react";
 
 export default function Home() {
   return (
     <>
       <title>Next Notes</title>
-      <main className="flex min-h-screen w-full justify-center items-center flex-col gap-2">
-        <h1>Next Notes</h1>
-        <Button>Get Starred</Button>
-      </main>
+      <div className="w-full md:w-2xl h-screen flex flex-col justify-start items-start">
+        <div className="flex flex-row justify-end items-center w-full gap-2">
+          <h1 className="mr-auto text-2xl font-semibold">Next Notes</h1>
+          <Button variant={"outline"}>
+            <Github />
+          </Button>
+          <Button>
+            <NotebookPen className="mr-1" /> New Note
+          </Button>
+        </div>
+      </div>
     </>
   );
 }
