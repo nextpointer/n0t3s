ec074e8269ea852c74a1379e6acb0e52884dcd7b
nextpointer <110530249+nextpointer@users.noreply.github.com>
2025-07-30 12:45:07 +0530

    feat: add home layout

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -3,6 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <>
+      <title>Next Notes</title>
       <main className="flex min-h-screen w-full justify-center items-center flex-col gap-2">
         <h1>Next Notes</h1>
         <Button>Get Starred</Button>
