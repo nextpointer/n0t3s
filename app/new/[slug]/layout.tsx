0ba51f8f48e4e232ec38fa38d70d48a5076dbd54
nextpointer <110530249+nextpointer@users.noreply.github.com>
2025-08-04 11:16:52 +0530

    feat: add layout dynamic page for notees

---
+++ app/new/[slug]/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import "../../globals.css";
+
+export const metadata: Metadata = {
+  title: "Next Notes",
+  description: "simple open source ai integrated notes app",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        <main className="flex min-h-screen w-full justify-center items-center flex-col gap-2 p-2">
+          {children}
+        </main>
+      </body>
+    </html>
+  );
+}
---
+++ app/new/[slug]/page.tsx
@@ -0,0 +1,10 @@
+const page = () => {
+  return (
+    <>
+      <title>New Note</title>
+      <h1>new note</h1>
+    </>
+  );
+};
+
+export default page;
