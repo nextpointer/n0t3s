124ea3b1af58688ca0ba4ebfcf803473c3abc79c
nextpointer <110530249+nextpointer@users.noreply.github.com>
2025-08-08 15:03:15 +0530

    refactor: change font to space grotesk

---
+++ app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import { Toaster } from "@/components/ui/sonner";
+import { Space_Grotesk } from "next/font/google";
+
+export const metadata: Metadata = {
+  title: "Next Notes",
+  description: "simple open source ai integrated notes app",
+};
+
+const sg = Space_Grotesk({
+  subsets: ["latin"],
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        <main
+          className={`flex h-[100dvh] w-full justify-center items-center flex-col gap-2 p-2 overflow-hidden ${sg.className}`}
+        >
+          {children}
+        </main>
+        <Toaster />
+      </body>
+    </html>
+  );
+}
